ca759de005087da0604d71cbdc6c9ba828717770
Matsuda Takuya <matsuda.takuya41@gmail.com>
2020-10-10 17:04:30 +0900

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,31 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: false,
+        bracketSpacing: true,
+        endOfLine: "auto",
+        trailingComma: "es5",
+      },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
 };
